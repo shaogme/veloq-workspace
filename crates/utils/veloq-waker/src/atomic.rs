e893a0a59caf35044d9dcc39a02a7a016109cb94
韶光 <87350612+shaogme@users.noreply.github.com>
2026-07-20 22:22:02 +0800

    remove unused docs.

--- crates/utils/veloq-waker/src/atomic.rs
+++ crates/utils/veloq-waker/src/atomic.rs
@@ -38,63 +38,6 @@ use crate::common::{
 ///
 /// For concurrent calls to `register` (should be avoided) the ordering is only
 /// guaranteed for the winning call.
-///
-/// # Examples
-///
-/// Here is a simple example providing a `Flag` that can be signalled manually
-/// when it is ready.
-///
-/// ``` ignore
-/// use futures::future::Future;
-/// use futures::task::{Context, Poll, AtomicWaker};
-/// use std::sync::Arc;
-/// use std::sync::atomic::AtomicBool;
-/// use std::sync::atomic::Ordering::Relaxed;
-/// use std::pin::Pin;
-///
-/// struct Inner {
-///     waker: AtomicWaker,
-///     set: AtomicBool,
-/// }
-///
-/// #[derive(Clone)]
-/// struct Flag(Arc<Inner>);
-///
-/// impl Flag {
-///     pub fn new() -> Self {
-///         Flag(Arc::new(Inner {
-///             waker: AtomicWaker::new(),
-///             set: AtomicBool::new(false),
-///         }))
-///     }
-///
-///     pub fn signal(&self) {
-///         self.0.set.store(true, Relaxed);
-///         self.0.waker.wake();
-///     }
-/// }
-///
-/// impl Future for Flag {
-///     type Output = ();
-///
-///     fn poll(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<()> {
-///         // quick check to avoid registration if already done.
-///         if self.0.set.load(Relaxed) {
-///             return Poll::Ready(());
-///         }
-///
-///         self.0.waker.register(cx.waker());
-///
-///         // Need to check condition **after** `register` to avoid a race
-///         // condition that would result in lost notifications.
-///         if self.0.set.load(Relaxed) {
-///             Poll::Ready(())
-///         } else {
-///             Poll::Pending
-///         }
-///     }
-/// }
-/// ```
 pub struct AtomicWaker {
     vtable: AtomicPtr<RawWakerVTable>,
     data: AtomicPtr<()>,
